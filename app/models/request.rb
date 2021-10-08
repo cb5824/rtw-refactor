2825e17e4b26fd5d5958eaadab168c0d8daba1e6
CB5824 <craig.baylis@gmail.com>
2021-10-08 19:55:25 +0000

    wrote methods for mp and cp spec tests

--- app/models/request.rb
+++ app/models/request.rb
@@ -15,4 +15,26 @@ class Request < ApplicationRecord
     return status
   end
 
+  def weekly_mp
+    all_mps = []
+    self.days.each do |day|
+      if day.cancelled == false
+        all_mps << day.mp1
+        all_mps << day.mp2
+      end
+    end
+    return "#{all_mps.min} -\n #{all_mps.max}"
+  end
+
+  def weekly_cp
+    all_cps = []
+    self.days.each do |day|
+      if day.cancelled == false
+        all_cps << day.cp1
+        all_cps << day.cp2
+      end
+    end
+    return "#{(CP_ARRAY.rassoc(all_cps.min))[0]} -\n #{(CP_ARRAY.rassoc(all_cps.max))[0]}"
+  end
+
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -30,7 +30,7 @@ feature 'requests' do
       request1.days[i].mp2 = 2.0
     end
     request1.days[4].mp1 = 3.0
-    expect(request1.weekly_mp).to eq('2.0 -\n 3.0')
+    expect(request1.weekly_mp).to eq("2.0 -\n 3.0")
 
     for i in 0..6 do
       request1.days[i].mp1 = 6.0
@@ -38,11 +38,11 @@ feature 'requests' do
     end
     request1.days[3].mp1 = 3.0
     request1.days[1].mp2 = 11.0
-    expect(request1.weekly_mp).to eq('3.0 -\n 11.0')
+    expect(request1.weekly_mp).to eq("3.0 -\n 11.0")
 
     request1.days[3].mp2 = 3.0
     request1.days[1].mp1 = 11.0
-    expect(request1.weekly_mp).to eq('3.0 -\n 11.0')
+    expect(request1.weekly_mp).to eq("3.0 -\n 11.0")
 
   end
 
@@ -52,18 +52,18 @@ feature 'requests' do
       request1.days[i].cp2 = 6
     end
     request1.days[4].cp1 = 26
-    expect(request1.weekly_cp).to eq('S.L. CP 4th -\n S.L. CP Ralston')
+    expect(request1.weekly_cp).to eq("S.L. CP 4th -\n S.L. CP Ralston")
     for i in 0..6 do
       request1.days[i].cp1 = 6
       request1.days[i].cp2 = 6
     end
     request1.days[4].cp1 = 3
     request1.days[4].cp2 = 11
-    expect(request1.weekly_cp).to eq('N.L. CP Common -\n N.L. CP Brisbane')
+    expect(request1.weekly_cp).to eq("N.L. CP Common -\n N.L. CP Brisbane")
 
     request1.days[4].cp2 = 3
     request1.days[4].cp1 = 11
-    expect(request1.weekly_cp).to eq('N.L. CP Common -\n N.L. CP Brisbane')
+    expect(request1.weekly_cp).to eq("N.L. CP Common -\n N.L. CP Brisbane")
 
   end
 
